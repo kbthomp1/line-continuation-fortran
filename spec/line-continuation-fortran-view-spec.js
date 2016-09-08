abac5b5c0401308bd243ec6e77f623ae95f13ee2
kbthomps <kyle.b.thompson@nasa.gov>
2016-09-08 16:06:30 -0400

    Remove view files

--- spec/line-continuation-fortran-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import LineContinuationFortranView from '../lib/line-continuation-fortran-view';
-
-describe('LineContinuationFortranView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
